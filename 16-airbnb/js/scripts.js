7305027e734cb6c23d07a74e90db7fc155906c85
cituk <programaciondyn3.mx@sandos.com>
2020-04-07 23:36:36 -0500

    javascript para el Footer

---
+++ 16-airbnb/js/scripts.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+    const btnFlotante = document.querySelector('.btn-flotante');
+    btnFlotante.addEventListener('click', (e)=> {
+        e.preventDefault();
+
+        const footer = document.querySelector('#footer');  
+
+        if(footer.classList.contains('activo')){
+            footer.classList.remove('activo');
+            btnFlotante.classList.remove('activo');
+            btnFlotante.innerText = 'Idioma y moneda';
+
+        }else{
+            footer.classList.add('activo');
+            btnFlotante.classList.add('activo');
+            btnFlotante.innerText = 'X Cerrar';
+        }
+
+    });
+});
