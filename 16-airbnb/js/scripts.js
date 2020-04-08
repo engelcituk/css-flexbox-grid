6baab41c8a4341a881498c72c6d3b1033360bfb2
cituk <programaciondyn3.mx@sandos.com>
2020-04-08 00:24:32 -0500

    ultimos ajustes al proyecto

--- 16-airbnb/js/scripts.js
+++ 16-airbnb/js/scripts.js
@@ -1,4 +1,32 @@
 document.addEventListener('DOMContentLoaded', ()=> {
+
+    //obtener la imagen
+    const imagenHero = document.querySelector('.hero');
+
+    //inicializar las variables
+    let i = 0;
+    let tiempo = 0;
+    // arreglo de imagenes de fondo
+    const imagenes = ['arriba2.jpg', 'arriba.jpg'];
+
+    setInterval( ()=> {
+
+        imagenHero.style.backgroundPositionY= '-'+tiempo+'px';
+
+        if(tiempo > 40){
+            tiempo = 0;
+            imagenHero.style.backgroundImage = "url(img/"+imagenes[i]+")";
+
+            if(i === imagenes.length - 1){
+                i = 0;
+            }else{
+                i++;
+            }
+            console.log(i);
+        }
+        tiempo++;
+    },100);
+
     const btnFlotante = document.querySelector('.btn-flotante');
     btnFlotante.addEventListener('click', (e)=> {
         e.preventDefault();
